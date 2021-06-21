e22b3e92fa0022d58aac7df7fe5e83b2a6af5a07
Fw-Franz <fwfranzk@gmail.com>
2021-06-21 15:51:04 -0400

    added new file that creates average sheet of frames

---
+++ Create_Frame_mean_sheet.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+root = tk.Tk()
+root.withdraw()
+
+m1_filepath,m2_filepath = filedialog.askopenfilename(initialdir=BASE_DIR,
+                title="Select files m1 and m2",
+                multiple=True,
+                filetypes=(("csv", "*.csv"),))
+
+m1=pd.read_csv(m1_filepath)
+m2=pd.read_csv(m2_filepath)
+
+m_mean_inter=pd.concat([m1,m2])
+
+m_mean=m_mean_inter.groupby(['PlateNum', 'WellNum', 'Day', 'Marker','Condition','PlateRow','PlateColumn','Date'], sort = False).mean()
+m_mean["Frame"]="ean"
+
+m_mean_filepath=m1_filepath.replace("_m1_","_mean_")
+print(m_mean_filepath)
+m_mean.to_csv(m_mean_filepath, index=True)
+
+# print(m_mean)
