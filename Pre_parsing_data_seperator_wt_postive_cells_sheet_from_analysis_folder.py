fc9afdeae3cd15fd2753f70791c7028101890b15
Fw-Franz <fwfranzk@gmail.com>
2021-09-16 15:51:09 -0400

    pre parser for positive cells as well

---
+++ Pre_parsing_data_seperator_wt_postive_cells_sheet_from_analysis_folder.py
@@ -0,0 +1,57 @@
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import time
+import os
+
+start_time = time.time()
+base_directory =  os.getcwd() + '\\'
+input_dir_base = base_directory
+
+#region Input parameters
+
+date='08_07_2021' # sth like '01_21_2021'
+
+input_fname_GFAP="GFAP.csv"
+input_fname_nuclei="Nuclei.csv"
+input_fname_nuclei="PositiveCells.csv"
+
+fname_strings=['LC3_BS_Plate','casp3_BS_edge_1cAMPrap_Plate']  # should end with "_Plate"
+
+#endregion
+
+save_dir_base=input_dir_base + date+ '//'
+
+CHECK_FOLDER = os.path.isdir(save_dir_base)
+# If folder doesn't exist, then create it.
+if not CHECK_FOLDER:
+    os.mkdir(save_dir_base)
+
+print('loading data ... ')
+
+df_GFAP=pd.read_csv(input_dir_base+input_fname_GFAP)
+df_nuclei=pd.read_csv(input_dir_base+input_fname_nuclei)
+df_positive=pd.read_csv(input_dir_base+input_fname_nuclei)
+
+
+def search_and_save(df_GFAP,df_nuclei,save_dir_base,fname_string):
+
+    save_dir_i=save_dir_base+fname_string+'//'
+
+    CHECK_FOLDER = os.path.isdir(save_dir_i)
+    # If folder doesn't exist, then create it.
+    if not CHECK_FOLDER:
+        os.mkdir(save_dir_i)
+
+    df_GFAP_i= df_GFAP.loc[df_GFAP['FileName_DisplayImage'].str.contains(fname_string, case=False)]
+    df_nuclei_i= df_nuclei.loc[df_nuclei['FileName_DisplayImage'].str.contains(fname_string, case=False)]
+    df_positive_i= df_positive.loc[df_positive['FileName_DisplayImage'].str.contains(fname_string, case=False)]
+
+    df_GFAP_i.to_csv(path_or_buf=save_dir_i + 'GFAP.csv',  index=None, header=True)
+    df_nuclei_i.to_csv(path_or_buf=save_dir_i + 'Nuclei.csv',  index=None, header=True)
+    df_positive_i.to_csv(path_or_buf=save_dir_i + 'PositiveCells.csv',  index=None, header=True)
+
+for fname_string in fname_strings:
+    search_and_save(df_GFAP,df_nuclei,save_dir_base,fname_string)
+
+print("\n time spent in minutes: %s" % round(((time.time() - start_time))/60, 1))
